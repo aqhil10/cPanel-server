315ca3edc3a15937c037e3228eaec9aad19daa7d
aqhil10 <39744406+aqhil10@users.noreply.github.com>
2018-05-30 02:56:20 +0530

    Update phpinfo.php

--- Resources/phpinfo.php
+++ Resources/phpinfo.php
@@ -1,1 +1,9 @@
+PHP has many environmental variables that you are able to update as needed. 
+For example, you may need to update your php memory_limit to prevent certain scripts from running out of memory. 
+In order to change these values, you must first be able to see what they are. 
+To do that, you will need to create a phpinfo page. 
+A phpinfo page shows you all of your php environment settings.
+
+A phpinfo page is simply a php page with the following code:
+
 <? phpinfo(); ?>
